a4ba771f80cfdfb36c3a7733137503e95d59ed13
Iswan Jumat <iswan@thinkcube.com>
2016-10-07 22:19:15 +0530

    fixed undefinde result issue

--- __mocks__/got.js
+++ __mocks__/got.js
@@ -1,18 +1,26 @@
 let mockData = null;
+let processNewsData = null;
 
 // then in our mock `got()` function, we can set the mockData as the body
 
 const got = function(url) {
-  let success = new Promise(function(resolve) {
+  if (url.indexOf('item') !== -1) {
+    return new Promise(function(resolve) {
+      resolve({ body: processNewsData });
+    });
+  }
+
+  return new Promise(function(resolve) {
     resolve({ body: mockData });
   });
-
-  return success;
 };
 
 // here, we're creating a new function called __setFakeData() which sets the mockData variable
 got.__setFakeData = (data) => {
   mockData = data;
+};
+got.__setProcessData = (data) => {
+  processNewsData = data;
 };
 
 module.exports = got;
